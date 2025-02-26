61d43dc71f4d44bfc51d83a56f7df97528660967
zhucj3 <zhucj3@lenovo.com>
2025-02-26 09:37:53 +0800

    feat: init project.

---
+++ JavaTutorial/TestFirstModule/src/com/zcj/test01/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.zcj.test01;
+
+// 单行注释
+/* 
+* 多行注释
+* */
+
+/**
+ * 文档注释
+ * @author zcj
+ * */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+        System.out.println("Hello, world");
+    }
+}
